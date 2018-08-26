31c1655b16bbcd9da7cfbfb6073f2cb41065514a
Olga Goremykina <lesscau@gmail.com>
2018-08-26 21:33:13 +0300

    add DeprecatedClass annotation

---
+++ src/main/java/quoters/DepreciationHandlerBeanFactoryPostProcessor.java
@@ -0,0 +1,25 @@
+package quoters;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+
+public class DepreciationHandlerBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory configurableListableBeanFactory) throws BeansException {
+        String[] names = configurableListableBeanFactory.getBeanDefinitionNames();
+        for (String name : names) {
+            BeanDefinition beanDefinition = configurableListableBeanFactory.getBeanDefinition(name);
+            String beanClassName = beanDefinition.getBeanClassName();
+            try {
+                Class<?> beanClass = Class.forName(beanClassName);
+                DeprecatedClass annotation = beanClass.getAnnotation(DeprecatedClass.class);
+                if(annotation != null){
+                    beanDefinition.setBeanClassName(annotation.newImpl().getName());
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
--- src/main/java/quoters/Main.java
+++ src/main/java/quoters/Main.java
@@ -5,6 +5,6 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
-        Thread.sleep(100);
+        context.getBean(Quoter.class).sayQuote();
     }
 }
